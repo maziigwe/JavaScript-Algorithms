5f3ceb097e3791e57034a606e50b5a85be89bddd
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-07-16 21:28:59 +0100

    pair counter modified

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 function getValifPair(total, items) {
   let pairCount = 0;
   let uniqueItems = new Set([...items]);
+  let sameColorItems = [];
 
-  console.log(uniqueItems);
+  uniqueItems.forEach((item) => {
+    sameColorItems = items.filter((a) => a === item);
+    pairCount += Math.floor(sameColorItems.length / 2);
+  });
+
+  console.log(pairCount);
   //return pairCount;
 }
 
-console.log(getValifPair(9, [10, 20, 20, 10, 30, 50, 10, 10]));
+console.log(getValifPair(9, [10, 20, 20, 10, 30, 50, , 60, 70, 50, 10, 10]));
