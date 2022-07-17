d8eff9f5568c47ebca7a57e140379cbb3ed5b9a7
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-07-17 17:26:12 +0100

    added memoization

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ class KnapSack {
 
 //Function solution
 function knapSack(list, maxCap, listIndex, memo) {
+  if (memo[maxCap][listIndex]) {
+    return memo[maxCap][listIndex];
+  }
+
   if (maxCap === 0 || listIndex < 0) {
     return { items: [], value: 0, weight: 0 };
   }
@@ -73,16 +77,29 @@ function knapSack(list, maxCap, listIndex, memo) {
   const valueWithItem = sackWithItem.value + list[listIndex].value;
   const valueWithoutItem = sackWithoutItem.value;
 
+  let resultSack;
   if (valueWithItem > valueWithoutItem) {
     const updatedStack = {
       items: sackWithItem.items.concat(list[listIndex]),
       value: sackWithItem.value + list[listIndex].value,
       weight: sackWithItem.weight + list[listIndex].weight,
     };
-    return updatedStack;
+    resultSack = updatedStack;
   } else {
-    return sackWithoutItem;
+    resultSack = sackWithoutItem;
   }
+
+  memo[maxCap][listIndex] = resultSack;
+
+  return resultSack;
+}
+
+function knapSackFn(items, maxCap, index) {
+  const memo = Array.from(Array(maxCap + 1), () =>
+    Array(items.length).fill(undefined)
+  );
+  console.log(memo);
+  console.log(knapSack(items, maxCap, index, memo));
 }
 
 const items = [
@@ -90,14 +107,5 @@ const items = [
   { name: "b", value: 6, weight: 8 },
   { name: "c", value: 10, weight: 3 },
 ];
-
-function knapSackFn(items, maxCap, index) {
-  let memo = Array.from(Array(maxCap + 1), () => {
-    Array(items.length).fill(undefined);
-  });
-
-  console.log(memo);
-  //knapSack(items, maxCap, index, memo);
-}
 
 console.log(knapSackFn(items, 8, items.length - 1));
