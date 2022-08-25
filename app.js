345f6ed707175ce4563c92c0072d6532204647e5
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-08-25 14:21:03 +0100

    isPalindrome

---
+++ app.js
@@ -0,0 +1,15 @@
+function isPalindrome(word){
+
+  const stringReversed = reverseWord(word) 
+  return  word===stringReversed
+}
+
+function reverseWord(word) {
+    let output = "";
+    for (let cha of word) {
+      output = cha + output;
+    }
+    return output;
+  }
+
+  console.log(isPalindrome("racecar"));
