23d2286acb3667ac54ee0983481aadfffd04b7c5
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-23 19:31:17 +0100

    Added comment to isAnagram.js

--- isAnagram.js
+++ isAnagram.js
@@ -2,6 +2,12 @@ function isAnagram(str1, str2) {
   return formatStr(str1) === formatStr(str2);
 }
 
+//1. remove all non-letters using regEx & replace function.
+//2. convert all to lowerCase.
+//3. split all characters into arrays.
+//4. sort alphabetically
+//5. join characters again to form a string
+//6. compare the two strings
 function formatStr(str) {
   return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 }
